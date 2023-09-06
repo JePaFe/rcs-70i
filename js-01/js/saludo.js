16c787af1bf9d618ac4f1f822f72ef118c05571a
Jean Paul Ferreira <jeanpaul@jepafe.net>
2023-09-06 15:58:27 -0300

    JS - 01

---
+++ js-01/js/saludo.js
@@ -0,0 +1,3 @@
+console.log("Hola, desde el script");
+// debugger;
+console.log(3 + 1 + 2);
---
+++ js-01/js/script.js
@@ -0,0 +1,12 @@
+let parrafo;
+// debugger;
+parrafo = document.getElementById("segundo-parrafo");
+// debugger;
+parrafo = 123;
+// debugger;
+
+// evento click del párrafo
+parrafo.onclick = function () {
+  console.log("Entro a la función");
+  //   alert("Hola");
+};
